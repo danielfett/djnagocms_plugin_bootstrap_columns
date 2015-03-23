7da2c01ec3e8fe9f608c7e486135818219c6d12b
JeremyTiki <JeremyTiki@gmail.com>
2015-03-23 09:51:51 -0400

    Moved admin into cms_plugins.py

--- cmsplugin_bootstrap_columns/cms_plugins.py
+++ cmsplugin_bootstrap_columns/cms_plugins.py
@@ -38,6 +38,29 @@ plugin_pool.register_plugin(BootstrapRowPlugin)
 
 class BootstrapColumnPlugin(CMSPluginBase):
     allow_children = True
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'element_style', 'element_id',
+                       'mobile_device_width', 'small_device_width',
+                       'medium_device_width', 'large_device_width')
+        }),
+        ('Hide on Viewport Size ', {
+            'fields': ('hide_on_mobile', 'hide_on_small', 'hide_on_medium',
+                       'hide_on_large')
+        }),
+        ('Viewport Offset', {
+            'fields': ('mobile_device_offset', 'small_device_offset',
+                       'medium_device_offset', 'large_device_offset')
+        }),
+        ('Viewport Pull', {
+            'fields': ('mobile_device_pull', 'small_device_pull',
+                       'medium_device_pull', 'large_device_pull')
+        }),
+        ('Viewport Push', {
+            'fields': ('mobile_device_push', 'small_device_push',
+                       'medium_device_push', 'large_device_push')
+        }),
+    )
     model = BootstrapColumn
     module = "Bootstrap"
     name = "Bootstrap Column"
